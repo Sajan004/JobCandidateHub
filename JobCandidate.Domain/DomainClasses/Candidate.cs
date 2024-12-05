491541b506d842951fa6c909514406c80d09c635
sazn <sazn_004@hotmail.com>
2024-12-05 21:01:42 +0545

    Add project files.

---
+++ JobCandidate.Domain/DomainClasses/Candidate.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidate.Domain.DomainClasses
+{
+    public class Candidate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = default!;
+
+        [Required]
+        public string FirstName { get; set; } = default!;
+
+        [Required]
+        public string LastName { get; set; } = default!;
+
+        public string? PhoneNumber { get; set; }
+
+        public string? BestCallTime { get; set; }
+
+        public string? LinkedInProfile { get; set; }
+
+        public string? GitHubProfile { get; set; }
+
+        [Required]
+        public string Comment { get; set; } = default!;
+    }
+}
