8b83fe108e9579a95c74e26356f48f57c3c28bf9
sazn <sazn_004@hotmail.com>
2024-12-05 21:46:40 +0545

    1. Create Data Libary Project 2. Create DBContext 3. Adding Connection String, Migration and setup Connection String in program.cs

---
+++ JobCandidate.Data/Context/JobCandidateDbContext.cs
@@ -0,0 +1,12 @@
+﻿using JobCandidate.Domain.DomainClasses;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobCandidate.Data.Context
+{
+    public class JobCandidateDbContext : DbContext
+    {
+        public JobCandidateDbContext(DbContextOptions<JobCandidateDbContext> options) : base(options) { }
+
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
--- JobCandidate.Domain/DomainClasses/Candidate.cs
+++ JobCandidate.Domain/DomainClasses/Candidate.cs
@@ -8,24 +8,24 @@ namespace JobCandidate.Domain.DomainClasses
         public int Id { get; set; }
 
         [Required]
-        [EmailAddress]
-        public string Email { get; set; } = default!;
+        public string FirstName { get; set; } = string.Empty!;
 
         [Required]
-        public string FirstName { get; set; } = default!;
-
-        [Required]
-        public string LastName { get; set; } = default!;
+        public string LastName { get; set; } = string.Empty!;
 
         public string? PhoneNumber { get; set; }
 
-        public string? BestCallTime { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string? CallTimeInterval { get; set; }
 
         public string? LinkedInProfile { get; set; }
 
         public string? GitHubProfile { get; set; }
 
         [Required]
-        public string Comment { get; set; } = default!;
+        public string Comment { get; set; } = string.Empty;
     }
 }
